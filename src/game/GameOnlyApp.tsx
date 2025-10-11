d817965be8f43cfa469bf1e248fa786f8b7b69c1
bhety.tech <bhety.tech@gmail.com>
2025-10-11 09:47:19 +0200

    Added css on gameMain.tsx

--- src/game/GameOnlyApp.tsx
+++ src/game/GameOnlyApp.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PaperFoldWarOnline from '@/pages/PaperFoldWarOnline';
+import Navbar from "@/components/Navbar.tsx";
+import Footer from "@/components/Footer.tsx";
 
 export default function GameOnlyApp() {
     return (
         <BrowserRouter>
+            <Navbar/>
             <Routes>
                 <Route path="/games/paper-fold-war/online" element={<PaperFoldWarOnline />} />
                 <Route path="/games/paper-fold-war/online/:roomId" element={<PaperFoldWarOnline />} />
             </Routes>
+            <Footer/>
         </BrowserRouter>
     );
 }
--- src/gameMain.tsx
+++ src/gameMain.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import GameOnlyApp from './game/GameOnlyApp';
-
-// If you need Tailwind styles in the game build, import your main CSS here:
+import './index.css'
 import './App.css';
 
 createRoot(document.getElementById('root')!).render(
