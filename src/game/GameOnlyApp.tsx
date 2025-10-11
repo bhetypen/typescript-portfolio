669ee31c6bde7de456d0a699633456802def6889
bhety.tech <bhety.tech@gmail.com>
2025-10-11 09:51:26 +0200

    mirror the app.tsx on game app

--- src/game/GameOnlyApp.tsx
+++ src/game/GameOnlyApp.tsx
@@ -2,12 +2,26 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PaperFoldWarOnline from '@/pages/PaperFoldWarOnline';
 import Navbar from "@/components/Navbar.tsx";
 import Footer from "@/components/Footer.tsx";
+import Home from "@/pages/Home.tsx";
+import ProjectsPage from "@/pages/Projects.tsx";
+import Privacy from "@/pages/Privacy.tsx";
+import Contact from "@/pages/Contact.tsx";
+import Games from "@/pages/Games.tsx";
+import ToolsPage from "@/pages/Tools.tsx";
+import PaperFoldWar from "@/pages/PaperFoldWar.tsx";
 
 export default function GameOnlyApp() {
     return (
         <BrowserRouter>
             <Navbar/>
             <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<ProjectsPage />} />
+                <Route path="/privacy" element={<Privacy/>}/>
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/games" element={<Games/>}/>
+                <Route path="/tools" element={<ToolsPage/>}/>
+                <Route path="/games/paper-fold-war" element={<PaperFoldWar/>}/>
                 <Route path="/games/paper-fold-war/online" element={<PaperFoldWarOnline />} />
                 <Route path="/games/paper-fold-war/online/:roomId" element={<PaperFoldWarOnline />} />
             </Routes>
